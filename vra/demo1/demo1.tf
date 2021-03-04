167be3fa36ca493c93a243e09fc0ba00a01d93b1
nsxlivefire <48851132+nsxlivefire@users.noreply.github.com>
2021-03-04 12:40:36 +0000

    Update demo1.tf

--- vra/demo1/demo1.tf
+++ vra/demo1/demo1.tf
@@ -1,1 +1,43 @@
+terraform {
+  required_providers {
+    nsxt = {
+      source = "vmware/nsxt"
+      version = "3.1.0"
+    }
+  }
+}
 
+provider "nsxt" {
+  host                  = "192.168.110.15"
+  username              = "admin"
+  password              = "VMware1!VMware1!"
+  allow_unverified_ssl  = true
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
+}
+
+resource "nsxt_policy_group" "VDI" {
+  nsx_id       = "VDI"
+  display_name = "VDI"
+  criteria {
+    condition {
+      member_type = "Segment"
+      key         = "Tag"
+      operator    = "EQUALS"
+      value       = "zone|vdi"
+    }
+  }
+  conjunction {
+    operator = "OR"
+  }
+  criteria {
+    condition {
+      key         = "Name"
+      member_type = "VirtualMachine"
+      operator    = "EQUALS"
+      value       = "win10-01a"
+    }
+  }
+}
